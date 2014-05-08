b218dc07d2bb56a287cfcae64968eddce79bb44d
yaniovejero <yaninaovejero89@gmail.com>
2014-05-08 12:14:17 -0300

    segunda revision agregando rutas internas

--- PracticoBundle/Controller/DefaultController.php
+++ PracticoBundle/Controller/DefaultController.php
@@ -6,8 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
+        $name="hola";
         return $this->render('ProgPracticoBundle:Default:index.html.twig', array('name' => $name));
     }
 }
